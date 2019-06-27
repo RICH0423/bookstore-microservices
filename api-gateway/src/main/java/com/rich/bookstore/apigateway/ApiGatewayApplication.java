f9c8a58d010b3678b3b55f24ff1b002152baf2ac
rich.lee <rich.lee@deltaww.com>
2019-06-27 11:04:14 +0800

    Add api-gateway

---
+++ api-gateway/src/main/java/com/rich/bookstore/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rich.bookstore.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
